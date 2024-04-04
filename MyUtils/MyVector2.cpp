d2370676d054036669d405a780bde09428d56e13
Serhii <serikgorbanev@gmail.com>
2024-04-04 03:06:59 +0200

    Added template struct MyVector2 which will replace MyVector2f and MyVector2i

---
+++ MyUtils/MyVector2.cpp
@@ -0,0 +1,164 @@
+#include "MyVector2.h"
+#include "MyMath.h"
+
+namespace MyUtils
+{
+    namespace MyMath
+    {
+        template <typename T>
+        template <typename U>
+        inline MyVector2<T>::MyVector2(const MyVector2<U>& vector) :
+            x(static_cast<T>(vector.x)),
+            y(static_cast<T>(vector.y))
+        {
+        }
+
+        template<typename T>
+        template<typename U>
+        inline float MyVector2<T>::distanceTo(const MyVector2<U>& otherVector)
+        {
+            return distance(static_cast<float>(x), static_cast<float>(y), static_cast<float>(otherVector.x), static_cast<float>(otherVector.y));
+        }
+
+        template<typename T>
+        template<typename U>
+        float MyVector2<T>::distanceTo(const U x2, const U y2)
+        {
+            return distance(static_cast<float>(x), static_cast<float>(y), static_cast<float>(x2), static_cast<float>(y2));
+        }
+
+        template<typename T>
+        template<typename U>
+        float MyVector2<T>::distanceSquaredTo(const MyVector2<U>& vector)
+        {
+            return distanceSquared(static_cast<float>(x), static_cast<float>(y), static_cast<float>(vector.x), static_cast<float>(vector.y));
+        }
+
+        template<typename T>
+        template<typename U>
+        float MyVector2<T>::distanceSquaredTo(U x2, U y2)
+        {
+            return distanceSquared(static_cast<float>(x), static_cast<float>(y), static_cast<float>(x2), static_cast<float>(y2));
+        }
+
+        template<typename T>
+        inline float MyVector2<T>::getLength()
+        {
+            return distance(static_cast<float>(x), static_cast<float>(y));
+        }
+
+        template<typename T>
+        float MyVector2<T>::length()
+        {
+            return getLength();
+        }
+
+        template<typename T>
+        float MyVector2<T>::len()
+        {
+            return getLength();
+        }
+
+        template<typename T>
+        inline float MyVector2<T>::getLengthSquared()
+        {
+            return distanceSquared(static_cast<float>(x), static_cast<float>(y));
+        }
+
+        template<typename T>
+        inline float MyVector2<T>::lengthSquared()
+        {
+            return getLengthSquared();
+        }
+
+        template<typename T>
+        inline float MyVector2<T>::lenSquared()
+        {
+            return getLengthSquared();
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator -(const MyVector2<T>& right)
+        {
+            return MyVector2<T>(-right.x, -right.y);
+        }
+
+        template <typename T>
+        inline MyVector2<T>& operator +=(MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            left.x += right.x;
+            left.y += right.y;
+
+            return left;
+        }
+
+        template <typename T>
+        inline MyVector2<T>& operator -=(MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            left.x -= right.x;
+            left.y -= right.y;
+
+            return left;
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator +(const MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            return MyVector2<T>(left.x + right.x, left.y + right.y);
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator -(const MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            return MyVector2<T>(left.x - right.x, left.y - right.y);
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator *(const MyVector2<T>& left, T right)
+        {
+            return MyVector2<T>(left.x * right, left.y * right);
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator *(T left, const MyVector2<T>& right)
+        {
+            return MyVector2<T>(right.x * left, right.y * left);
+        }
+
+        template <typename T>
+        inline MyVector2<T>& operator *=(MyVector2<T>& left, T right)
+        {
+            left.x *= right;
+            left.y *= right;
+
+            return left;
+        }
+
+        template <typename T>
+        inline MyVector2<T> operator /(const MyVector2<T>& left, T right)
+        {
+            return MyVector2<T>(left.x / right, left.y / right);
+        }
+
+        template <typename T>
+        inline MyVector2<T>& operator /=(MyVector2<T>& left, T right)
+        {
+            left.x /= right;
+            left.y /= right;
+
+            return left;
+        }
+
+        template <typename T>
+        inline bool operator ==(const MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            return (left.x == right.x) && (left.y == right.y);
+        }
+
+        template <typename T>
+        inline bool operator !=(const MyVector2<T>& left, const MyVector2<T>& right)
+        {
+            return (left.x != right.x) || (left.y != right.y);
+        }
+    }
+}
---
+++ MyUtils/MyVector2.h
@@ -0,0 +1,104 @@
+#pragma once
+
+namespace MyUtils
+{
+    namespace MyMath
+    {
+        template <typename T>
+        struct MyVector2
+        {
+        public:
+            T x;
+            T y;
+
+            template <typename T>
+            inline MyVector2() :
+                x(0),
+                y(0)
+            {}
+
+            template <typename T>
+            inline MyVector2(T X, T Y) :
+                x(X),
+                y(Y)
+            {}
+
+            template <typename U>
+            MyVector2(const MyVector2<U>& vector);
+
+            template <typename U>
+            float distanceTo(const MyVector2<U>& vector);
+            template <typename U>
+            float distanceTo(U x2, U y2);
+
+            template <typename U>
+            float distanceSquaredTo(const MyVector2<U>& vector);
+            template <typename U>
+            float distanceSquaredTo(U x2, U y2);
+
+            inline float getLength();
+            inline float length();
+            inline float len();
+
+            inline float getLengthSquared();
+            inline float lengthSquared();
+            inline float lenSquared();
+        };
+
+        template <typename T>
+        MyVector2<T> operator -(const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T>& operator +=(MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T>& operator -=(MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T> operator +(const MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T> operator -(const MyVector2<T>& left, const MyVector2<T>& right);
+
+
+        template <typename T>
+        MyVector2<T> operator *(const MyVector2<T>& left, T right);
+
+        template <typename T>
+        MyVector2<T> operator *(T left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T>& operator *=(MyVector2<T>& left, T right);
+
+        template <typename T>
+        MyVector2<T> operator /(const MyVector2<T>& left, T right);
+
+        template <typename T>
+        MyVector2<T>& operator /=(MyVector2<T>& left, T right);
+
+
+        template <typename T>
+        MyVector2<T> operator *(const MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T>& operator *=(MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T> operator /(const MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        MyVector2<T>& operator /=(MyVector2<T>& left, const MyVector2<T>& right);
+
+
+        template <typename T>
+        bool operator ==(const MyVector2<T>& left, const MyVector2<T>& right);
+
+        template <typename T>
+        bool operator !=(const MyVector2<T>& left, const MyVector2<T>& right);
+
+        typedef MyVector2<int>          Vector2i;
+        typedef MyVector2<unsigned int> Vector2u;
+        typedef MyVector2<double>       Vector2d;
+        typedef MyVector2<float>        Vector2f;
+    }
+}

5c9aa20d1c60ad444d6a2c22a42eb68be44d1fe2
Serhii <serikgorbanev@gmail.com>
2024-05-04 23:56:04 +0200

    Added VectorTrigonometry.cpp
    Also moved PolarToCartesian function from Vector2<T> to the new file.

--- MyUtils/MyVector2.cpp
+++ MyUtils/MyVector2.cpp
@@ -10,13 +10,5 @@ namespace MyUtils
         {
             return "MyVector2(" + std::to_string(x) + ", " + std::to_string(y) + ")";
         }
-
-        template<typename T>
-        MyVector2<T> MyVector2<T>::PolarToCartesian(float length, float angleRadians)
-        {
-            float x = cos(angleRadians) * length;
-            float y = sin(angleRadians) * length;
-            return MyVector2<T>(x, y);
-        }
     }
 }
---
+++ MyUtils/VectorTrigonometry.cpp
@@ -0,0 +1,26 @@
+#include "MyVector2.h"
+#include "Angle.h"
+
+namespace MyUtils
+{
+	namespace MyMath
+	{
+		namespace AngleUtils
+		{
+			/// <summary>
+			/// Calculates MyVector2 from it's angle and length
+			/// </summary>
+			/// <returns>Vector with coordinates 
+			///x = cos(angle) * length,
+			///y = sin(angle) * length
+			///</returns>
+			template <typename T>
+			MyVector2<T> PolarToCartesian(float length, Angle angle)
+			{
+				float x = cos(angle.Radians()) * length;
+				float y = sin(angle.Radians()) * length;
+				return MyVector2<T>(x, y);
+			}
+		}
+	}
+}
